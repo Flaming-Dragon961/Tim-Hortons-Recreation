ddfdc6a14e3a2c861066284a288f6e7f8b486078
Flaming-Dragon961 <aab936406@gmail.com>
2025-10-07 15:50:56 -0400

    Added verification
    Added verification for sign up page to see if the first name and email are filled.

--- signup.js
+++ signup.js
@@ -1,4 +1,29 @@
 function buttonclick(){
-    alert("Your account has been created!")
+    
 
+    fname = document.getElementById("fname")
+    fnamevalue = fname.value
+    email = document.getElementById("email")
+    emailvalue = email.value
+
+    if (fnamevalue.length===0 && emailvalue.length>=1){
+
+        alert("Please fill out the first name section")
+    }
+
+    else if (emailvalue.length===0 && fnamevalue.length>=1){
+
+        alert("Please fill out your email")
+    }
+
+    else if (fnamevalue.length===0 && emailvalue.length===0){
+
+        alert("Please fill out your first name and email")
+    }
+
+    else{
+
+        alert("Your account has been created!")
+
+    }
 }
